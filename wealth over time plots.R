3db852aac34b002864d770165d9350c4599e2f89
Nikita Pavlov <nikita.pavlov.sva@hotmail.com>
2022-12-23 16:55:52 +0100

    got CE plot graph correct

--- wealth over time plots.R
+++ wealth over time plots.R
@@ -17,7 +17,7 @@ source("Utility Functions.R")
 
 #=============== Section 2: load functions ========
 
-get_terminal_wealth_perScenario <- function(opt_allocation,return_list){
+get_terminal_wealth_perScenario <- function(opt_allocation,return_list,max_horizon){
   
   Nscenarios <- nrow(return_list[[1]])
   
@@ -35,7 +35,7 @@ get_terminal_wealth_perScenario <- function(opt_allocation,return_list){
   return(wealth[,max_horizon+1])
 }
 
-get_wealth_perScenario <- function(opt_allocation,return_list){
+get_wealth_perScenario <- function(opt_allocation,return_list,max_horizon){
   
   Nscenarios <- nrow(return_list[[1]])
   
@@ -50,19 +50,29 @@ get_wealth_perScenario <- function(opt_allocation,return_list){
     }
     wealth[,t+1] <- wealth[,t]*return
   }
-  return(wealth[,max_horizon+1])
+  return(wealth)
 }
 
-get_CE <- function(opt_allocation,return_list){
-  wealth_build_up <- get_terminal_wealth_perScenario(opt_allocation,return_list)
+get_CE <- function(opt_allocation,return_list,max_horizon){
+  wealth_build_up <- get_terminal_wealth_perScenario(opt_allocation,return_list,max_horizon)
   
   CE_rational <- CE(wealth_build_up,gamma = 5)
   
   return(CE_rational)
 }
 
-CE_plot <- function(){
+CE_plot <- function(CE_over_horizons){
   
+  CE_graph <- cbind(1:15,stack(CE_over_horizons[1:ncol(CE_over_horizons)]))
+  colnames(CE_graph) = cbind('Horizon', 'CE', 'Strategy')
+
+  # stacked area chart
+  ggplot(CE_graph, aes(x=Horizon, y=CE,fill=Strategy,color=Strategy)) + 
+    theme_classic() +
+    labs(y= "Certainty Equivalent Rate", x = "Horizon") +
+    geom_line() +
+    scale_x_continuous(breaks = c(3,6,9,12,15))+
+    scale_color_manual(values=c("red", "blue", "green"))
 }
 
 #=============== Section 3: 1 over N allocation, with equal weight across asset classes ========
@@ -73,8 +83,31 @@ equity_allocations <- as.data.frame(matrix(round(0.25/8,2),nrow=15,ncol=8))
 oneOverN_allocation_equityFair <- cbind(bond_allocations,equity_allocations)
 colnames(oneOverN_allocation_equityFair) <- assets
 
-oneOverN_allocation_equityFair_horizons <- list()
-CE_oneOverN_horizons <- as.data.frame(matrix(0,nrow=15,ncol=1)) 
+CE_oneOverNFair_horizons <- as.data.frame(matrix(0,nrow=15,ncol=1))
+colnames(CE_oneOverNFair_horizons) <- "oneOverNFair"
 for (horizon in 1:15){
-  CE_oneOverN_horizons[horizon,1] <- get_CE(oneOverN_allocation_equityFair[1:horizon,],return_test_set)
+  CE_oneOverNFair_horizons[horizon,1] <- get_CE(oneOverN_allocation_equityFair[1:horizon,],return_test_set,horizon)
+  if (horizon==15){
+    wealthPerScenario_oneOverNFair <- get_wealth_perScenario(oneOverN_allocation_equityFair[1:horizon,],return_test_set,horizon)
+  }
 }
+
+#=============== Section 4: 1 over N allocation, with equal weight across all assets ========
+
+oneOverN_allocation <- as.data.frame(matrix(round(1/length(assets),2),nrow=15,ncol=11))
+colnames(oneOverN_allocation) <- assets
+
+CE_oneOverN_horizons <- as.data.frame(matrix(0,nrow=15,ncol=1))
+colnames(CE_oneOverN_horizons) <- "oneOverN"
+for (horizon in 1:15){
+  CE_oneOverN_horizons[horizon,1] <- get_CE(oneOverN_allocation[1:horizon,],return_test_set,horizon)
+  if (horizon==15){
+    wealthPerScenario_oneOverN <- get_wealth_perScenario(oneOverN_allocation[1:horizon,],return_test_set,horizon)
+  }
+}
+
+#=============== Section 5: Create CE plot with all asset allocations ========
+
+CE_all <- cbind(CE_oneOverNFair_horizons,CE_oneOverN_horizons)
+
+CE_plot(CE_all)
